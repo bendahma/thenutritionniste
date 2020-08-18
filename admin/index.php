66345442f4bbb38585a4aae63b9930f8a7d8554d
kaddour bendahma mohamed el-amine <contact@kbma.com>
2020-08-18 16:04:27 +0200

    admin ui update

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
 ?>
    
     <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column container">
+    <div id="content-wrapper" class="d-flex flex-column ">
 
       <!-- Main Content -->
       <div id="content">
@@ -70,12 +70,11 @@
            
           </div>
 
-          <!-- Content Row -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Les Recettes </h1>
           </div>
           <div class="row">
-            <div class="col-xl-7 col-lg-7">
+            <div class="col-xl-6 col-lg-7">
               <div class="card shadow mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">Ajouter une recette</h6>
@@ -125,39 +124,46 @@
               </div>
             </div>
 
-            <div class="col-xl-5 col-lg-5">
+            <div class="col-xl-6 col-lg-5">
               <div class="card shadow mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">LISTES DES RECETTES</h6>
                   <div class="dropdown no-arrow">
                     <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                     </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                      <div class="dropdown-header">Dropdown Header:</div>
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
+                    
                   </div>
                 </div>
-                <!-- Card Body -->
                 <div class="card-body">
                   <div class="chart-pie pt-4 pb-2">
-                    <canvas id="myPieChart"></canvas>
-                  </div>
-                  <div class="mt-4 text-center small">
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-primary"></i> Direct
-                    </span>
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-success"></i> Social
-                    </span>
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-info"></i> Referral
-                    </span>
-                  </div>
+                    <table class="table table-bordered">
+                      <thead class="thead-dark">
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Titre</th>
+                          <th scope="col">Image</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td >1</td>
+                          <td >Reccete title Reccete title Reccete title Reccete </td>
+                          <td><img src="" alt=""></td>
+                          <td>
+                            <select name="" id="" class="custom-select">
+                              <option value="">Choisi</option>
+                              <option value="">Affiche</option>
+                              <option value="">Edit</option>
+                              <option value="">Delete</option>
+                            </select>
+                          </td>
+                          
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>                  
                 </div>
               </div>
             </div>
@@ -167,7 +173,7 @@
           </div>
           <!-- Content Row -->
           <div class="row">
-            <div class="col-xl-7 col-lg-7">
+            <div class="col-xl-6 col-lg-7">
               <div class="card shadow mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">Ajouter un régime</h6>
@@ -210,36 +216,51 @@
                 </div>
               </div>
             </div>
-            
-
-            <div class="col-lg-5 mb-4">
-
-              <!-- Illustrations -->
+            <div class="col-xl-6 col-lg-5">
               <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-                </div>
-                <div class="card-body">
-                  <div class="text-center">
-                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">LISTES DES RECETTES</h6>
+                  <div class="dropdown no-arrow">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                    </a>
+                    
                   </div>
-                  <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
-                  <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>
-                </div>
-              </div>
-
-              <!-- Approach -->
-              <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
                 </div>
                 <div class="card-body">
-                  <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS bloat and poor page performance. Custom CSS classes are used to create custom components and custom utility classes.</p>
-                  <p class="mb-0">Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.</p>
+                  <div class="chart-pie pt-4 pb-2">
+                    <table class="table table-bordered">
+                      <thead class="thead-dark">
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Titre</th>
+                          <th scope="col">Image</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td >1</td>
+                          <td >Reccete title Reccete title Reccete title Reccete </td>
+                          <td><img src="" alt=""></td>
+                          <td>
+                            <select name="" id="" class="custom-select">
+                              <option value="">Choisi</option>
+                              <option value="">Affiche</option>
+                              <option value="">Edit</option>
+                              <option value="">Delete</option>
+                            </select>
+                          </td>
+                          
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>                  
                 </div>
               </div>
-
             </div>
+
+            
           </div>
 
         </div>
